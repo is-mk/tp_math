9509eec3e16c9a732052edad3a02bbe204c9074f
theot <theotalmawows@gmail.com>
2026-04-13 16:15:38 +0200

    rajoue des includes dans le main

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 #include "include/justeprix.h"
 #include "include/nombre.h"
 #include "include/problemes.h"
+#include "include/addition.h"
+#include "include/soustraction.h"
+#include "include/Multiplication.h"
+#include "include/table_de_mutliplication.h"
 
 int main(void) {
     printf("Hello, World!\n");
