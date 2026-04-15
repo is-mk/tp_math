3f9c03afdce6216bf040c4038c84904ed20aa66d
islem <islemmaki11@gmail.com>
2026-04-15 11:11:26 +0200

    ajout juste prix

--- src/justeprix.c
+++ src/justeprix.c
@@ -3,7 +3,34 @@
 //
 
 #include "../include/justeprix.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void justeprix() {
-    return;
+int justeprix() {
+    int essaie = 0;
+
+    srand(time(NULL));
+
+    int nombre = rand() % 100 + 1;
+    int proposition = 101;
+
+    while (nombre != proposition) {
+        printf("Quel est votre proposition ? (1-100)");
+        scanf("%d", &proposition);
+
+        if (proposition == nombre) {
+            printf("Bravo vous avez trouvé le bon nombre !");
+        } else {
+            printf("Mauvaise réponse..");
+            essaie++;
+            if (nombre < proposition) {
+                printf("Le nombre à trouver est plus bas.");
+            } else {
+                printf("Le nombre à trouver est plus haut.");
+            }
+        }
+    }
+
+    return essaie;
 }
