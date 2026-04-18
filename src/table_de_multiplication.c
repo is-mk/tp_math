e7ab13fb8d5dde407274b3dd9ee403365c73f0e6
theot <theotalmawows@gmail.com>
2026-04-19 00:02:54 +0200

    les tables DES MOULTIPLICATSSIONNNNNE finito bon courage pour le reste chef

--- src/table_de_multiplication.c
+++ src/table_de_multiplication.c
@@ -4,10 +4,39 @@
 
 #include "../include/table_de_multiplication.h"
 #include <stdio.h>
-#include <stdlib.h>
+
 
 int table_de_multiplication() {
     int essaie = 0;
-    int n, réponse;
+    int t, réponse;
 
+    //choix de la table
+
+    do {
+        printf("Choisissez la table que vous souhaitez apprendre (de 1 à 10)");
+        scanf("%d", &t);
+    } while (t < 1 || t > 10);
+
+    printf("\n Table de %d \n", t);
+
+    //affichage table
+
+    for (int i = 1; i <= t; i++) {
+        printf("%d x %d = %d \n", t, i, i * t);
+    }
+
+    //partie test ou quiz
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = ?",t, i);
+        scanf("%d",&réponse);
+
+        if (réponse != i * t) {
+            essaie ++;
+            printf("Mauvaise réponse.");
+        }
+        else {
+            printf("Bonne réponse");
+        }
+    }
+    return essaie;
 }
